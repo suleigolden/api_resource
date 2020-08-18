19aa5b6818799629c7c0b5a08b21d02eda26b12d
Suleiman Mamman <suleimanmamman@Suleimans-MacBook-Pro.local>
2020-08-18 19:34:01 +0100

    can create a student part 2

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Student;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Student;
+
 
 class StudentController extends Controller
 {
     
-    public function store()
+    public function store(Request $request)
     {
-        return response()->json([], 201);
+
+        $result = Student::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'course' => $request->course,
+            'email' => $request->email,
+        ]);
+
+        return response()->json($result, 201);
     }
 }
--- tests/Feature/Api/StudentControllerTest.php
+++ tests/Feature/Api/StudentControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Api;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Faker\Factory;
 
 class StudentControllerTest extends TestCase
 {
@@ -16,12 +17,23 @@ class StudentControllerTest extends TestCase
     public function test_can_create_a_student()
     {
         $this->withoutExceptionHandling();
+        
+        $faker = Factory::create();
         $response = $this->json('POST','/api/store/student', [
+            
+            'first_name' => $fname = $faker->name,
+            'last_name' => $lname = $faker->name,
+            'course' => $faker->word,
+            'email' => $faker->unique()->safeEmail
 
         ]);
 
 
-        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'first_name','last_name','course','email'
+        ])
+        
+        ->assertStatus(201);
 
 
     }
